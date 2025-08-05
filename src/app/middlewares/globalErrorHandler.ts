0557185ec105de28a5df4d0dc2909edb3e2fc7b6
firoz.nur <firoznur5@gmail.com>
2025-08-05 17:33:24 +0600

    Add : Mongo DB Error Handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -8,10 +8,18 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  console.log(err);
+
   let statusCode = 500;
   let message = `Something went wrong ${err.message} from global middleware`;
-
-  if (err instanceof AppError) {
+  if (err.code === 11000) {
+    statusCode = 409;
+    // message = `Duplicate ${Object.keys(err.keyValue)} entered, ${Object.values(err.keyValue)} already exists`;
+    message = `${Object.values(err.keyValue)} already exists`;
+  } else if (err.name === "CastError") {
+    statusCode = 400;
+    message = "Invalid ID, Please Provide A Valid ID";
+  } else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
   } else if (err instanceof Error) {
